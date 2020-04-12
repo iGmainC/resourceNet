f73d32d6f3140bbabfd1856cae17ec7231794d58
九年吃菜粥 <guo199909075419@foxmail.com>
2020-04-12 18:13:19 +0800

    aa

--- book/models.py
+++ book/models.py
@@ -5,15 +5,33 @@ import json
 class Book(models.Model):
     id = IntegerField(primary_key=True)
     name = CharField(max_length=100,null=True)
-    author = CharField(max_length=100)
+    author = CharField(max_length=100,null=True)
     file_idm = CharField(max_length=100,null=True)
     douban_id = CharField(max_length=115,null=True)
+    cover_img_url = CharField(max_length=300,null = True)
+    brief = CharField(max_length=1000,null = True)
     epub_download_url = CharField(max_length=300,null=True)
     mobi_download_url = CharField(max_length=300,null=True)
     pdf_download_url = CharField(max_length=300,null=True)
     azw3_download_url = CharField(max_length=300,null=True)
-    cover_img_url = CharField(max_length=300,null = True)
     date = DateTimeField()
 
     def __str__(self):
         return self.name
+
+    def toDict(self):
+        return {
+        "id":self.id,
+        "name":self.name,
+        "author":self.author,
+        "file_idm":self.file_idm,
+        "douban_id":self.douban_id,
+        "cover_img_url":self.cover_img_url,
+        "brief":self.brief,
+        "epub_download_url":self.epub_download_url,
+        "mobi_download_url":self.mobi_download_url,
+        "pdf_download_url":self.pdf_download_url,
+        "azw3_download_url":self.azw3_download_url,
+        "date":self.date}
+    def toJson(self):
+        return json.dumps(self.toDict(),ensure_ascii=False)
--- book/views.py
+++ book/views.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+import time
 from django.views.decorators.csrf import csrf_exempt
+from book.models import Book
 
 # Create your views here.
-
 def index(request):
     return render(request,'book/index.html')
 
 @csrf_exempt
 def postbox(request):
     if request.method == "POST":
-        d = json.load(request.body)
+        d = json.loads(request.body)
+        d_l = d['name'].split('.')[0]
+        name,format = d_l[0],d_l[1]
+        date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        if Book().objects.filter(name=name):
+            b = Book().objects.get(name=name)
+            if format == 'epub':
+                b.epub_download_url = d['url']
+            if format == 'azw3':
+                b.azw3_download_url = d['url']
+            if format == 'pdf':
+                b.pdf_download_url = d['url']
+            if format == 'mobi':
+                b.mobi_download_url = d['url']
+        else:
+            b = Book(name = name,
+                 file_idm=d['idm'],
+                 date=date)
+            if format == 'epub':
+                b.epub_download_url = d['url']
+            if format == 'azw3':
+                b.azw3_download_url = d['url']
+            if format == 'pdf':
+                b.pdf_download_url = d['url']
+            if format == 'mobi':
+                b.mobi_download_url = d['url']
+        b.save()
